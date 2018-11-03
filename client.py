b53d1a4dcd002e2db1f0e44a3bf9e211d8d373d4
Pablo Muguerza <pmuguer@gmail.com>
2018-11-03 16:15:17 -0300

    Emprolijado el código

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import socket
 
 client_sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#client_sck.connect(("127.0.0.1", 9090))
 client_sck.connect(("127.0.0.1", 9090))
+
 
 ASCII_11 = b'\x0B'
 ASCII_28 = b'\x1C'
@@ -15,20 +17,23 @@ def encode_message(msg):
     retval = SB + msg + EB + CR
     return retval
 
-client_messages = ["Hola mundo 1", "Hola mundo 2", "Hola mundo 3"]
-
-for client_message in client_messages:
-    encoded_message = encode_message(client_message)
-    client_sck.send(encoded_message)
-    server_data = client_sck.recv(1024)
-    server_message = ""
-    for idx in range(0, len(server_data)):
-        if server_data[idx] == SB:
-            server_message = ""
-        elif server_data[idx] == EB and server_data[idx+1] == CR:
-            print server_message
-        else:
-            if server_data[idx] != CR:
-                server_message = server_message + server_data[idx]
-
-client_sck.close()
+if __name__ == "__main__":
+    client_messages = ["Hola mundo 1", "Hola mundo 2", "Hola mundo 3"]
+    
+    for client_message in client_messages:
+        encoded_message = encode_message(client_message)
+        print "Enviando mensaje (en formato MLLP): -> '{}'".format(client_message)
+        client_sck.send(encoded_message)
+        server_data = client_sck.recv(1024)
+        server_message = ""
+        for idx in range(0, len(server_data)):
+            if server_data[idx] == SB:
+                server_message = ""
+            elif server_data[idx] == EB and server_data[idx+1] == CR:
+                print "Respuesta recibida del server: '{}'".format(
+                    server_message)
+            else:
+                if server_data[idx] != CR:
+                    server_message = server_message + server_data[idx]
+    
+    client_sck.close()
--- server.py
+++ server.py
@@ -1,9 +1,8 @@
+#! -*- coding: utf-8 -*-
 import socket
 
-server_sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_sck.bind(("127.0.0.1", 9090))
-server_sck.listen(10)
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 9090
 
 ASCII_11 = b'\x0B'
 ASCII_28 = b'\x1C'
@@ -13,27 +12,39 @@ SB = ASCII_11
 EB = ASCII_28
 CR = ASCII_13
 
-def encode_message(msg):
-    retval = SB + msg + EB + CR
-    return retval
-
-conn, addr = server_sck.accept()
-print "connected by: {}".format(addr)
-while True:
-    client_data = conn.recv(1024)
-    if not client_data:
-        break
-    client_message = ""
-    for idx in range(0, len(client_data)):
-        if client_data[idx] == SB:
-            client_message = ""
-        elif client_data[idx] == EB and client_data[idx+1] == CR:
-            number = client_message[-1]
-            print client_message
-            client_message = ""
-            server_response = "Ok " + str(number)
-            encoded_server_response = encode_message(server_response)
-            conn.send(encoded_server_response)
-        else:
-            if client_data[idx] != CR:
-                client_message = client_message + client_data[idx]
+if __name__ == "__main__":
+    server_sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sck.bind((SERVER_HOST, SERVER_PORT))
+    print "Server listening at: {}:{}".format(SERVER_HOST, SERVER_PORT)
+    print ""
+    server_sck.listen(10)
+    
+    def encode_message(msg):
+        retval = SB + msg + EB + CR
+        return retval
+    
+    conn, addr = server_sck.accept()
+    print "connected by: {}".format(addr)
+    print ""
+    while True:
+        client_data = conn.recv(1024)
+        if not client_data:
+            break
+        client_message = ""
+        for idx in range(0, len(client_data)):
+            if client_data[idx] == SB:
+                client_message = ""
+            elif client_data[idx] == EB and client_data[idx+1] == CR:
+                number = client_message[-1]
+                print "Mensaje recibido del cliente '{}'".format(
+                    client_message)
+                client_message = ""
+               
+                 
+                server_response = "Ok {}".format(number)
+                encoded_server_response = encode_message(server_response)
+                print "Enviando respuesta (en formato MLLP) -> '{}'".format(server_response)
+                conn.send(encoded_server_response)
+            else:
+                if client_data[idx] != CR:
+                    client_message = client_message + client_data[idx]
