beec8e140cd6361f2826c0a65b0c21e689f04449
Pablo Muguerza <pmuguer@gmail.com>
2018-11-05 01:13:48 -0300

    Comentarios

--- groovy/mllp.groovy
+++ groovy/mllp.groovy
@@ -1,9 +1,14 @@
 public class mllp {
+    // Helper class con los caracteres y un metodo
+    // de conveniencia para generar mensajes usando MLLP
     static START_BLOCK = 11
     static END_BLOCK = 28
     static CARRIAGE_RETURN = 13
 
     static byte[] create_mllp_message(String message) {
+        // Generar, a partir de un String, un array
+        // de caracteres que incluye los separadores
+        // requeridos por MLLP
         def mllp_message = []
         def msg_list = message.getBytes().toList()
         mllp_message.addAll([this.START_BLOCK])
