537502212eeda04e957b10a60230b252ec27413f
Pablo Muguerza <pmuguer@gmail.com>
2018-11-18 23:39:17 -0300

    Refactorizado el agregado del segemto PID con los datos del paciente

--- tarea_2/src/ADTMessage.groovy
+++ tarea_2/src/ADTMessage.groovy
@@ -11,30 +11,32 @@ import ca.uhn.hl7v2.model.v25.segment.PID
 import ca.uhn.hl7v2.model.v25.segment.PV1
 import ca.uhn.hl7v2.model.v25.datatype.ST
 
+// Cliente para enviar mensajes MLLP al server
 import MLLPClient
 
 class ADTMessage {
+    // Clase que simplifica la creación de un mensaje ADT, usando HAPI
+    // para la generación del mensaje y los segmentos que lo componen
 
-    String patient
     ADT_A01 adt
 
     // Constructor, recibe el paciente para el que se crearán los mensajes
-    def ADTMessage(patient, dateTimeOfMessage, messageControlID, locationMap, admitDateTime) {
-        // Genera un nuevo mensaje ADT, para el paciente patient
-        // patient:
+    def ADTMessage(patient, dateTimeOfMessage, messageControlID, location, admitDateTime) {
+        // Genera un nuevo mensaje ADT, con los siguientes datos:
+        // patient: mapa con los datos del paciente
         // dateTimeOfMessage: string en formato "YYYYMMDDHHMMSS"
         // messageControlID: string con id que identificará el mensaje
-        this.patient = patient
+        // location: mapa con los datos de la ubicación de la internación
+        // admitDateTime: string en formato "YYYYMMDDHHMMSS"
 
         // Genero una nueva instancia de un mensaje ADT
-        //ADT_A01 adt = new ADT_A01()
         this.adt = new ADT_A01()
        
         // Genero el MSH con los atributos que van en todos los mensajes 
         this.initMSH()
 
-        // Indico el paciente
-        this.initPatient()
+        // Inicializo el segmento PID con los datos del paciente
+        this.initPIDSegment(patient)
 
         // Registro el timestamp del mensaje
         this.adt.getMSH().getDateTimeOfMessage().getTime().setValue(dateTimeOfMessage)
@@ -42,7 +44,7 @@ class ADTMessage {
         this.adt.getMSH().getMessageControlID().setValue(messageControlID)
 
         // Registro los datos del segmento PV1 (patient visit)
-        this.setPV1Data(admitDateTime, locationMap)
+        this.setPV1Data(admitDateTime, location)
     }
 
     def initMSH() {
@@ -85,23 +87,23 @@ class ADTMessage {
         mshSegment.getProcessingID().getProcessingMode().setValue("T")
     }
 
-    def initPatient() {
+    def initPIDSegment(patient) {
         // Registro los datos del paciente en el segmento PID
         PID pid = this.adt.getPID()
         
         // Registro apellido, nombre, fecha de nacimiento y sexo
-        pid.getPatientName(0).getFamilyName().getSurname().setValue("Houssay")
-        pid.getPatientName(0).getGivenName().setValue("Bernardo")
+        pid.getPatientName(0).getFamilyName().getSurname().setValue(patient["surName"])
+        pid.getPatientName(0).getGivenName().setValue(patient["givenName"])
         // Fecha de nacimiento: 21/08/1921
-        pid.getDateTimeOfBirth().getTime().setValue("19200821")
-        pid.getAdministrativeSex().setValue("M")
+        pid.getDateTimeOfBirth().getTime().setValue(patient["dateTimeOfBirth"])
+        pid.getAdministrativeSex().setValue(patient["administrativeSex"])
        
         // Registro id0: Identificador único dentro del sistema público del GCBA
-        pid.getPatientIdentifierList(0).getAssigningAuthority().getNamespaceID().setValue("HCE_GCBA")
-        pid.getPatientIdentifierList(0).getIDNumber().setValue("201324")
+        pid.getPatientIdentifierList(0).getAssigningAuthority().getNamespaceID().setValue(patient["id0"]["assigningAuthority"])
+        pid.getPatientIdentifierList(0).getIDNumber().setValue(patient["id0"]["idNumber"])
         // Registro id1: Identificador único para Argentina (DNI, asignado por RENAPER)
-        pid.getPatientIdentifierList(1).getAssigningAuthority().getNamespaceID().setValue("RENAPER")
-        pid.getPatientIdentifierList(1).getIDNumber().setValue("10324121")
+        pid.getPatientIdentifierList(1).getAssigningAuthority().getNamespaceID().setValue(patient["id1"]["assigningAuthority"])
+        pid.getPatientIdentifierList(1).getIDNumber().setValue(patient["id1"]["idNumber"])
     }
 
     def setPV1Data(admitDateTime, locationMap) {
--- tarea_2/src/example_messages.groovy
+++ tarea_2/src/example_messages.groovy
@@ -1,5 +1,25 @@
 import ADTMessage
 
+// Registro id0: Identificador único dentro del sistema público del GCBA
+def id0 = [:]
+id0["assigningAuthority"] = "HCE_GCBA"
+id0["idNumber"] = "201324"
+
+// Registro id1: Identificador único para Argentina (DNI, asignado por RENAPER)
+def id1 = [:]
+id1["assigningAuthority"] = "RENAPER"
+id1["idNumber"] = "10324121"
+
+def patientMap = [:]
+// Registro apellido, nombre, fecha de nacimiento y sexo
+patientMap["surName"] = "Houssay"
+patientMap["givenName"] = "Bernardo"
+// Fecha de nacimiento: 21/08/1921
+patientMap["dateTimeOfBirth"] = "19200821"
+patientMap["administrativeSex"] = "M"
+patientMap["id0"] = id0
+patientMap["id1"] = id1
+
 def locationMap = [:]
 locationMap["nameSpaceID"] = "SistemaPublicoGCBA"
 locationMap["universalID"] = "HospArgerich"
@@ -14,6 +34,5 @@ locationMap["bed"] = "301"
 // * con ubicación del ingreso locationMap
 // * con fecha y hora del ingreso = "20181116081500"
 
-def patient = ""
-adtMessage = new ADTMessage(patient, "20181115121200", "1000", locationMap, "20181116081500")
+adtMessage = new ADTMessage(patientMap, "20181115121200", "1000", locationMap, "20181116081500")
 adtMessage.sendMessage()
